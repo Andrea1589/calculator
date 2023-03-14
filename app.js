37d719c931cc75711f1f6889292dfe6e7109eb34
Andrea Carrion <andreaecs@gmail.com>
2023-03-14 17:01:34 -0500

    fix: add comments

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Define the operate function to perform an arithmetic operation
+// based on the operator string and two numbers
 function operate(operator, a, b){
   let result = 0;
   switch (operator) {
@@ -18,24 +20,28 @@ function operate(operator, a, b){
       break;
   
     default:
+      console.log('Invalid operator');
       break;
   }
   return result;
 }
 
-
+// Define the add function of two numbers
 function add(a, b){
   return a + b;
 }
 
+// Define the substract function of two numbers
 function substract(a, b){
   return a - b;
 }
 
+// Define the multiply function of two numbers
 function multiply(a, b){
   return a * b;
 }
 
+// Define the divide function of two numbers
 function divide(a, b){
   if (b === 0) {
     return "No se puede dividir para cero";
